01764bb4d0682f9eeb1fcf0aee6b06b483b87c5c
nozomuiw <nozomuiw@gmail.com>
2014-03-14 22:47:28 +0900

    会社情報ページ作成

--- common/js/siiis.js
+++ common/js/siiis.js
@@ -1,13 +1,13 @@
 $(function(){
-	 $('#srch_menu h4').click(function() {
-            $(this).next().slideToggle();
-            $(this).find('a').toggleClass('clicked');
-      });
+	$('#srch_menu h4').click(function() {
+	    $(this).next().slideToggle();
+	    $(this).find('a').toggleClass('clicked');
+	});
 });
 
 $(function() {
-    $('#dropdown_nav li').find('.sub_nav').hide();
-    $('#dropdown_nav li').hover(function() {
-    	$(this).find('.sub_nav').slideToggle();
-    });
+	$('#dropdown_nav li').find('.sub_nav').hide();
+	$('#dropdown_nav li').hover(function() {
+		$(this).find('.sub_nav').slideToggle();
+	});
 });
